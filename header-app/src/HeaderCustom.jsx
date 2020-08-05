204a2a60e2ee35cdc40a2c0c2a18b4b59ec6eb8c
Adam Chen Wei <adamchenwei@gmail.com>
2020-08-05 12:17:30 -0400

    chore(): tweak

--- header-app/src/HeaderCustom.jsx
+++ header-app/src/HeaderCustom.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Detail from 'detail/Detail';
-const Header = () => <div style={{ backgroundColor: 'ForestGreen'}}>HEADER here ALso... <Detail/> (In header)see anythinig lefT?</div>;
+const Header = () => <div style={{ backgroundColor: 'ForestGreen'}}>HEADER here ALso... <Detail colorName="MediumSlateBlue"/> (In header)see anythinig lefT?</div>;
 
 export default Header;
